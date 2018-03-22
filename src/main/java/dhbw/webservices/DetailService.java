d89ebf89c9de7301be4b469a5898020bbe1d9521
Moritz Bührer <30311027+moritzbuehrer@users.noreply.github.com>
2018-03-22 19:53:46 +0100

    detail service

--- src/main/java/dhbw/webservices/DetailService.java
+++ src/main/java/dhbw/webservices/DetailService.java
@@ -1,18 +1,122 @@
 package dhbw.webservices;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dhbw.pojo.detail.album.DetailsAlbum;
+import dhbw.pojo.detail.artist.DetailsArtist;
+import dhbw.pojo.detail.track.DetailsTrack;
+import dhbw.pojo.result.search.SearchResultList;
+import dhbw.spotify.RequestCategory;
+import dhbw.spotify.RequestType;
+import dhbw.spotify.SpotifyRequest;
+import dhbw.spotify.WrongRequestTypeException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class DetailService {
 
     @RequestMapping("/detail/{id}")
-    public void detail(@PathVariable(value = "id") String id, @RequestParam(value = "type") String type){
-        System.out.println(id + " " + type);
+    public String detail(@PathVariable(value = "id") String id, @RequestParam(value = "type") String type) {
+
+        RequestCategory category = null;
+        Optional<String> result = null;
+        switch (type) {
+            case "ARTIST": {
+                category = RequestCategory.ARTIST;
+                break;
+            }
+            case "ALBUM": {
+                category = RequestCategory.ALBUM;
+                break;
+            }
+            case "TRACK": {
+                category = RequestCategory.TRACK;
+                break;
+            }
+            default: {
+                System.out.println(type);
+                return "";
+            }
+        }
+
+        SpotifyRequest request = new SpotifyRequest(RequestType.DETAIL);
+
+        try {
+            result = request.performeRequestDetail(category,id);
+        } catch (WrongRequestTypeException e) {
+            e.printStackTrace();
+        }
+
+        if (result.isPresent()) {
+            String json = result.get();
+
+            switch (category) {
+                case ALBUM: {
+                    searchResultList = processAlbum(json);
+                    break;
+                }
+                case TRACK: {
+                    searchResultList = processTrack(json);
+                    break;
+                }
+                case ARTIST: {
+                    searchResultList = processArtist(json);
+                    break;
+                }
+            }
 
 
+        }
+
+        return "";
+
+    }
+
+    private List<SearchResultList> processArtist(String json) {
+        ObjectMapper mapper = new ObjectMapper();
+        DetailsArtist artist = null;
+
+        try {
+            artist = mapper.readValue(json, DetailsArtist.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    private List<SearchResultList> processAlbum(String json) {
+        ObjectMapper mapper = new ObjectMapper();
+        DetailsAlbum album = null;
+
+        try {
+            album = mapper.readValue(json, DeatilsAlbum.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    private List<SearchResultList> processTrack(String json) {
+        ObjectMapper mapper = new ObjectMapper();
+        DetailsTrack track = null;
+
+        try {
+            track = mapper.readValue(json, DetailsTrack.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
