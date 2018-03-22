94dc2357356d7338e2f496469313630a7540326b
Moritz Bührer <30311027+moritzbuehrer@users.noreply.github.com>
2018-03-22 19:23:23 +0100

    get requestparam

--- src/main/java/dhbw/webservices/DetailService.java
+++ src/main/java/dhbw/webservices/DetailService.java
@@ -3,14 +3,15 @@ package dhbw.webservices;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DetailService {
 
     @RequestMapping("/detail/{id}")
-    public void detail(@PathVariable(value = "id") String id){
-        System.out.println(id);
+    public void detail(@PathVariable(value = "id") String id, @RequestParam(value = "type") String type){
+        System.out.println(id + " " + type);
 
 
     }
