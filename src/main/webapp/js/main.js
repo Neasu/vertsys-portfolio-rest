4281005d374c97d7205fdbcc711030a9aa2283c6
Moritz Bührer <30311027+moritzbuehrer@users.noreply.github.com>
2018-03-28 18:48:25 +0200

    Final commit

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -32,25 +32,14 @@ $(document).ready(() => {
 
 
                 $.getJSON(urlDetailsThis, (responseDetail) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    alert(responseDetail.title + ':\n' + responseDetail.info);
-                })
-=======
-=======
->>>>>>> 860045ad930440abc30c2655a88e7153c7d63a7a
-                    //alert(responseDetail.title + ':\n' + responseDetail.info);
 
+                    //alert(responseDetail.title + ':\n' + responseDetail.info);
 
                     $('#insertInfo').html('Titel: ' + responseDetail.title + '<br>' + 'Infos: ' + responseDetail.info);
                     $('.modal').modal();
                     $('#modal1').modal('open');
 
                 });
-<<<<<<< HEAD
->>>>>>> 860045ad930440abc30c2655a88e7153c7d63a7a
-=======
->>>>>>> 860045ad930440abc30c2655a88e7153c7d63a7a
             });
 
             $('#resultList').on('click', '#playButton', event=> {
@@ -83,7 +72,5 @@ $(document).ready(() => {
 
 
     });
-
-
 
 })
